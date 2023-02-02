8e8b5b088ffcf89ad5e511213526c3c693b4ac56
Jonathan Craigmyle <102497316+JohnnyValor@users.noreply.github.com>
2023-02-02 10:03:37 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { Provider } from 'react-redux';
 import App from './components/App';
 import reducer from './reducers/counter';
 
+// This code creates a Redux store with the provided 'reducer' function.
+// The second argument 'undefined' specifies the initial state of the store.
+// The third argument is a store enhancer, which enables the use of the Redux DevTools extension in the browser if it is available, otherwise it just returns the store as is.
 const store = createStore(
   reducer,
   undefined,
